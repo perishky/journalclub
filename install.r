590cd6de1b7f956f1a1df74085e732fa3b3461ce
Matthew Suderman <matthewsuderman@hotmail.com>
2019-09-20 14:44:53 +0100

    removed some skeletons

--- install.r
+++ install.r
@@ -1,5 +1,4 @@
 #' Only a few steps are needed to install journalclub in R:
 install.packages("devtools")
 library(devtools)
-source("http://bioconductor.org/biocLite.R")
 devtools::install_github("perishky/journalclub")
