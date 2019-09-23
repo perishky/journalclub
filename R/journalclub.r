9c51d6aa25744a2bb7a1d87c73bc43df5ae7af0c
Matthew Suderman <matthewsuderman@hotmail.com>
2019-09-23 18:37:15 +0100

    exposes functions

--- R/journalclub.r
+++ R/journalclub.r
@@ -14,14 +14,14 @@ save.output <- function(filename, lines) {
     return(TRUE)
 }
 
-#' Retrieve new papers for a journal club
+#' Retrieve new publications for a journal club
 #'
 #' @param dir Directory where the journal club history is stored.
-#' @param query PubMed query for identifying new papers (Default: NULL).
+#' @param query PubMed query for identifying new publications (Default: NULL).
 #' @param recent Number of days to look back for publications from the current day (Default: 30).
-#' @return A data frame listing recently published papers
+#' @return A data frame listing recent publications
 #' that have not been considered for previous journal clubs
-#' and either match the input query or cite a previously presented paper.
+#' and either match the input query or cite a previously presented publication.
 #' @export
 journalclub.candidates <- function(dir, query=NULL, recent=30, debug=F) {
     if (debug)
@@ -40,42 +40,47 @@ journalclub.candidates <- function(dir, query=NULL, recent=30, debug=F) {
         query.pmids <- NULL
     }
     else
-        query.pmids <- retrieve.pmids.by.query(query, days=recent)
+        query.pmids <- journalclub.query(query, days=recent)
 
     if (length(presented) == 0) {
-        warning("There are no previously presented papers to check for citations")
+        warning("There are no previously presented publications to check for citations")
         cite.pmids <- NULL
     }
     else
-        cite.pmids <- retrieve.citing.pmids(presented, days=recent)
+        cite.pmids <- journalclub.citing(presented, days=recent)
 
     new.pmids <- setdiff(c(query.pmids, cite.pmids), c(presented, ignore))
     if (length(new.pmids) == 0)
         NULL
     else {
-        new.papers <- retrieve.papers(new.pmids)
+        new.papers <- journalclub.annotate(new.pmids)
         save.output(file.path(dir, "ignore.txt"), new.pmids)
         new.papers
     }
 }
 
-#' Update journal club history with newly presented papers
+#' Update journal club history with newly presented publications
 #'
 #' @param dir Directory where the journal club history is stored.
-#' @param presented List of PMIDs for the papers that were presented.
+#' @param presented List of PMIDs for the publications that were presented.
 #' @return TRUE
+#' 
 #' @export
 journalclub.update <- function(dir, presented) {
     save.output(file.path(dir, "presented.txt"), presented)
 }
 
 
-#' Retrieve information (title, authors, abstract, etc)
-#' for the given paper pmids.
-#' `retmax` limits the number of papers to be retrieved
-#' at a time (but does not limit the total number of papers
-#' to retrieve).
-retrieve.papers <- function(pmids, retmax=100) {
+#' Retrieve information for a selection of publications
+#'
+#' Generates a data frame providing information (abstract, title, authors, journal, publication date)
+#' for a list of PMIDs.
+#'
+#' @param pmids A vector of PMIDs.
+#' @return A data frame with one row per publication.
+#' 
+#' @export
+journalclub.annotate <- function(pmids, retmax=100) {
     pmids <- na.omit(as.integer(gsub(" ", "", pmids)))
     
     papers <- lapply(seq(1,length(pmids),retmax), function(start) {
@@ -85,7 +90,7 @@ retrieve.papers <- function(pmids, retmax=100) {
         papers <- fetch_pubmed_data(object, retmax=retmax)
     
         papers <- articles_to_list(papers)
-        papers <- lapply(1:length(papers), function(i) {
+        papers <- mclapply(1:length(papers), function(i) {
             cat(date(), "retrieving paper", start+i-1, "of", length(pmids), "\n", file=stderr())
             paper <- papers[[i]]
             df <- article_to_df(paper, max_chars=-1, getKeywords=T)
@@ -104,10 +109,16 @@ retrieve.papers <- function(pmids, retmax=100) {
 }
 
 
-#' retrieve pmids for papers
-#' that match the input query
-#' within the few (default: 30) days
-retrieve.pmids.by.query <- function(query,days=30,retmax=1e5) {
+#' Query PubMed for recent publications
+#'
+#' Retrieve PMIDs for recent publications matching a query.
+#'
+#' @param query PubMed query.
+#' @param days Number of days to look back for publications from the current day (Default: 30).
+#' @return A vector of PMIDs matching the query.
+#' 
+#' @export
+journalclub.query <- function(query,days=30,retmax=1e5) {
     cat(date(), "Querying PubMed:", query, "\n")
     pubmed.url <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed"
     query <- gsub(" ", "+", as.character(query))
@@ -131,10 +142,16 @@ retrieve.pmids.by.query <- function(query,days=30,retmax=1e5) {
 }
 
 
-#' retrieve pmids for papers that cite
-#' the papers with input pmids within the last
-#' few (default: 30) days
-retrieve.citing.pmids <- function(pmids, days=30, retmax=100, verbose=T) {
+#' Query PubMed for recent citing publications
+#'
+#' Retrieve recent publications that cite a given set of publications
+#'
+#' @param pmids PMIDs for papers to be cited.
+#' @param days Number of days to look back for publications from the current day (Default: 30).
+#' @return A vector of PMIDs matching the query.
+#'
+#' @export
+journalclub.citing <- function(pmids, days=30, retmax=100, verbose=T) {
     pubmed.url <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&linkname=pubmed_pubmed_citedin"
     
     mindate <- format(Sys.Date()-days, "%Y/%m/%d")
